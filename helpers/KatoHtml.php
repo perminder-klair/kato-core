96ca7c42e68ba3cac813f61aa32389ce6d9eed4e
Parminder Klair <p.klair@scorchsoft.com>
2014-03-05 14:10:23 +0000

    added page routing

--- helpers/KatoHtml.php
+++ helpers/KatoHtml.php
@@ -2,7 +2,20 @@
 
 namespace kato\helpers;
 
+use yii\web\BadRequestHttpException;
+
 class KatoHtml extends \yii\base\Object
 {
-    
+	/**
+	 * Generates link to page
+	 * Usage: \kato\helpers\KatoHtml::page('page-slug');
+	 */
+    public static function page($slug = null)
+    {	
+    	if (is_null($slug)) {
+    		throw new BadRequestHttpException('Page slug not specified.');
+    	}
+
+    	return \Yii::$app->urlManager->createAbsoluteUrl('page/view', 'slug' => $slug);
+    }
 }
