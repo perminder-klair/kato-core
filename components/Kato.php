8ea6596fd0e558d5ed977223efa479fd238f83a4
Parminder Klair <p.klair@scorchsoft.com>
2014-03-05 11:20:28 +0000

    Added Kato Component

---
+++ components/Kato.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace kato\components;
+
+use Yii;
+use yii\db\Schema;
+use yii\base\Application;
+use backend\models\Setting;
+
+/**
+ * Usage
+ * -----
+ *
+ * In your configuration file, add the setting component.
+ *
+ * ```php
+ * 'components' => [
+ *	...
+ *	'kato' => 'kato\components\Kato',
+ *	...
+ * ]
+ * ```
+ */
+
+class Kato extends \yii\base\Component 
+{
+	/**
+	 * Get settings for Kato
+	 * Usage:
+	 *
+	 * ```php
+	 * $setting = Yii::$app->kato->setting('site_name');
+	 * ```
+	 */
+	public static function setting($key)
+    {
+        $model = Setting::find()
+            ->where(['define' => $key])
+            ->one();
+
+        if ($model) {
+            return $model->value;
+        }
+
+        return false;
+    }
+}
