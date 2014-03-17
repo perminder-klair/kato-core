869dac775e07638429a7022493e8ccfd4a1f4411
Parminder Klair <perminder.klair@gmail.com>
2014-03-17 15:02:28 +0000

    Added model relation to activerecords

--- ActiveRecord.php
+++ ActiveRecord.php
@@ -4,6 +4,28 @@ namespace kato;
 
 class ActiveRecord extends \yii\db\ActiveRecord
 {
+
+    /**
+     * Attached Content Media, by type
+     * @return static
+     */
+    public function getContentMedia()
+    {
+        return $this->hasMany(\backend\models\ContentMedia::className(), ['content_id' => 'id'])
+            ->where('media_type = :type', [':type' => $this->className()]);
+    }
+
+    /**
+     * Relate Media
+     * Usage: $model->media();
+     * @return static
+     */
+    public function getMedia()
+    {
+        return $this->hasMany(\backend\models\Media::className(), ['id' => 'media_id'])
+            ->via('contentMedia');
+    }
+
     /**
      * Actions to be taken before saving the record.
      * @param bool $insert
