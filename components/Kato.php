57f33d2a9a7c5b7754b8ee3cc4061cba802a1f5e
parminder <perminder.klair@gmail.com>
2014-05-09 16:30:03 +0100

    fix render block

--- components/Kato.php
+++ components/Kato.php
@@ -101,7 +101,7 @@ class Kato extends \yii\base\Component
     public function renderBlock($content = null)
     {
         if (!is_array($content)) {
-            if (empty($content) || $content == '') {
+            if (is_array(json_decode($content,true))) {
                 return false;
             }
             $content = Json::decode($content);
