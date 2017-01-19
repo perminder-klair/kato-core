168227027b857c90e8c5f24a2d591796b49ac9e7
Steven Waters <steven.waters@scorchsoft.com>
2017-01-19 11:52:14 +0000

    confirm button for delete page

--- widgets/views/_pageTreeItem.php
+++ widgets/views/_pageTreeItem.php
@@ -1,12 +1,16 @@
 <?php
-    use yii\helpers\Html;
+use yii\helpers\Html;
+use \yii\bootstrap\Modal;
 ?>
 <?php foreach ($dataProvider as $data): ?>
     <div class="pages-tree-item">
         - <?= Html::a($data['model']->title, ['update', 'id' => $data['model']->id], ['class' => 'title-link']) ?> (<small><?= $data['model']->id; ?></small>)
         <div class="tree-btns">
             <?= Html::a('Edit', ['update', 'id' => $data['model']->id], ['class' => 'btn btn-primary btn-xs']) ?>
-            <?= Html::a('Delete', ['delete', 'id' => $data['model']->id], ['class' => 'btn btn-primary btn-xs']) ?>
+            <?= Html::a('Delete', ['delete', 'id' => $data['model']->id], ['class' => 'btn btn-primary btn-xs popup-modal', 'data-toggle' => 'modal',
+                'data-target' => '#modal',
+                'data-id' => $data['model']->id,
+                'id' => 'popupModal-'. $data['model']->id]) ?>
         </div>
         <?php if ($data['children']): ?>
             <?php echo $this->render('_pageTreeItem', [
@@ -15,3 +19,29 @@
         <?php endif; ?>
     </div>
 <?php endforeach; ?>
+
+<?php Modal::begin([
+    'header' => '<h2 class="modal-title"></h2>',
+    'id'     => 'modal-delete',
+    'footer' => ''.Html::a('Cancel', '#', ['class' => 'btn btn-primary btn-xs modal-close', 'data-dismiss' => "modal"]).''.Html::a('Delete', ['delete', 'id' => 0], ['class' => 'btn btn-primary btn-xs', 'id' => 'modaldeletelink']),
+]); ?>
+
+<?= 'Are you sure you wish to delete this page?'; ?>
+
+<?php Modal::end(); ?>
+
+<?php
+
+$this->registerJs("$(function() {
+$('.popup-modal').click(function(e) {
+    e.preventDefault();
+    var modal = $('#modal-delete').modal('show');
+    modal.find('.modal-body').load($('.modal-dialog'));
+    var that = $(this);
+    var id = that.data('id');
+    modal.find('.modal-title').text('Delete page ' + id+ '?');
+    modal.find('.modal-footer').find('#modaldeletelink').attr('href', 'delete?id='+id);
+});
+});");
+
+?>
